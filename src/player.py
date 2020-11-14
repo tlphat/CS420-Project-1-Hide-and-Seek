f4b9b8d1e04704caa97221057c09f23bdcc91ca2
hnkhoi <khoi2410@gmail.com>
2020-11-14 20:53:56 +0700

    clean code

--- src/player.py
+++ src/player.py
@@ -12,15 +12,16 @@ class Player:
                 print("{:d}".format(cell), end = " ")
             print()
 
-    def isInside(self, i, j):
+    def isInsideMap(self, i, j):
         return (0 <= i and i < self.n and 0 <= j and j < self.m)
 
     def isInsideRange(self, id, i, j):
-        if not(abs(i - self.cell[id][0]) <= self.range and abs(j - self.cell[id][1]) <= self.range):
+        obj = self.cell[id]
+        if not(abs(i - obj[0]) <= self.range and abs(j - obj[1]) <= self.range):
             return False
         
-        if i >= self.cell[id][0] and j >= self.cell[id][1]:
-            u, v = i - self.cell[id][0], j - self.cell[id][1]
+        if i >= obj[0] and j >= obj[1]:
+            u, v = i - obj[0], j - obj[1]
 
     def updateLocation(self, id, i , j):
         self.cell[id] = [i, j]
@@ -31,35 +32,35 @@ class Player:
     # <MOVING> -----------------------------------------
 
     def up(self, id):
-        if (self.isInside(self.cell[id][0] - 1, self.cell[id][1])):
+        if (self.isInsideMap(self.cell[id][0] - 1, self.cell[id][1])):
             self.updateLocation(id, self.cell[id][0] - 1, self.cell[id][1])
 
     def down(self, id):
-        if (self.isInside(self.cell[id][0] + 1, self.cell[id][1])):
+        if (self.isInsideMap(self.cell[id][0] + 1, self.cell[id][1])):
             self.updateLocation(id, self.cell[id][0] + 1, self.cell[id][1])
 
     def left(self, id):
-        if (self.isInside(self.cell[id][0], self.cell[id][1] - 1)):
+        if (self.isInsideMap(self.cell[id][0], self.cell[id][1] - 1)):
             self.updateLocation(id, self.cell[id][0], self.cell[id][1] - 1)
 
     def right(self, id):
-        if (self.isInside(self.cell[id][0], self.cell[id][1] + 1)):
+        if (self.isInsideMap(self.cell[id][0], self.cell[id][1] + 1)):
             self.updateLocation(id, self.cell[id][0], self.cell[id][1] + 1)
 
     def leftUp(self, id):
-        if (self.isInside(self.cell[id][0] - 1, self.cell[id][1] - 1)):
+        if (self.isInsideMap(self.cell[id][0] - 1, self.cell[id][1] - 1)):
             self.updateLocation(id, self.cell[id][0] - 1, self.cell[id][1] - 1)
             
     def rightUp(self, id):
-        if (self.isInside(self.cell[id][0] - 1, self.cell[id][1] + 1)):
+        if (self.isInsideMap(self.cell[id][0] - 1, self.cell[id][1] + 1)):
             self.updateLocation(id, self.cell[id][0] - 1, self.cell[id][1] + 1)
 
     def leftDown(self, id):
-        if (self.isInside(self.cell[id][0] + 1, self.cell[id][1] - 1)):
+        if (self.isInsideMap(self.cell[id][0] + 1, self.cell[id][1] - 1)):
             self.updateLocation(id, self.cell[id][0] + 1, self.cell[id][1] - 1)
 
     def rightDown(self, id):
-        if (self.isInside(self.cell[id][0] + 1, self.cell[id][1] + 1)):
+        if (self.isInsideMap(self.cell[id][0] + 1, self.cell[id][1] + 1)):
             self.updateLocation(id, self.cell[id][0] + 1, self.cell[id][1] + 1)
 
     # </MOVING> -----------------------------------------
