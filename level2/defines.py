29a4e2fb5c2c2e03a457d5f865abd5f30779dc23
Phat <tlphat18@apcs.vn>
2020-12-06 16:42:32 +0700

    Restructure folders

--- level2/defines.py
+++
@@ -1,9 +0,0 @@
-class Config:
-    EMPTY, WALL, HIDER, SEEKER, OBS, ANNOUNCE, VERIFIED, IMPOSSIBLE = [0, 1, 2, 3, 4, 5, 6, 7]
-    DIR = [(-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)]
-    LEFT_UP, UP, RIGHT_UP, RIGHT, DOWN_RIGHT, DOWN, LEFT_DOWN, LEFT = DIR
-    RANGE_SEEKER = 3
-    RANGE_HIDER = 2
-    TIME_DELAY = 10
-    CELL_SIZE = 40
-    SIGNAL_HEURISTIC = 99999999
